65ee8a8fed9b1837d27c29b4065884f005090b23
Samantha Guergenenov <samguergen@gmail.com>
2017-03-16 17:30:24 -0400

    clicked on jquery

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ angular.module('myApp', [])
 .controller('AppCtrl', function($scope){
   console.log('inside the controller');
 
+
+
+})
+
+$(document).ready(function(){
+	$('button').click(function(){
+	console.log('clicked');
+	})	
 })
