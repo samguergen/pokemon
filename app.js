596e32270422a615ef81f032391a874d858e7613
Samantha Guergenenov <samguergen@gmail.com>
2017-03-16 18:11:39 -0400

    ngrepeat

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ angular.module('myApp', [])
 .controller('AppCtrl', function($scope){
   console.log('inside the controller');
 
+  $scope.inBag = [
+	{name: 'pikachu',
+	description: "the most popular",
+	evoluted: false,
+	imagePath: "img/pikachu.png"},
+
+	{name: 'squirtle',
+	description: "the aquatic",
+	evoluted: false,
+	imagePath: "img/squirtle.png"},
+
+	{name: 'jelly',
+	description: "the aquatic",
+	evoluted: false,
+	imagePath: "img/jelly.jpg"},
+
+	{name: 'dots',
+	description: "the aquatic",
+	evoluted: false,
+	imagePath: "img/dots.jpg"},
+  ];
 
 
 })
