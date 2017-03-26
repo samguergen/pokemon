2428dac6a0fbdcfd247277a0eea4ce608763129e
Samantha Guergenenov <samguergen@gmail.com>
2017-03-26 16:03:30 -0400

    indent and comms

--- app.js
+++ app.js
@@ -1,92 +1,94 @@
 angular.module('myApp', [])
 .controller('AppCtrl', function($scope){
-  console.log('inside the controller');
 
-  $scope.inBag = [
-	{name: 'Pikachu',
-	description: "The most popular",
-	type: "pokemon",
-	evolved: false,
-	imagePath: "img/pikachu.png"},
+	//Pokemon/items already present in bag
+ 	$scope.inBag = [
+		{name: 'Pikachu',
+		description: "The most popular",
+		type: "pokemon",
+		evolved: false,
+		imagePath: "img/pikachu.png"},
 
-	{name: 'Squirtle',
-	description: "The aquatic",
-	type: "pokemon",
-	evolved: false,
-	imagePath: "img/squirtle.png"},
+		{name: 'Squirtle',
+		description: "The aquatic",
+		type: "pokemon",
+		evolved: false,
+		imagePath: "img/squirtle.png"},
 
-	{name: 'Jelly beans',
-	description: "Yummy beans for your Pokemon.",
-	type: "candy",
-	evolved: false,
-	imagePath: "img/jelly.jpg"},
+		{name: 'Jelly beans',
+		description: "Yummy beans for your Pokemon.",
+		type: "candy",
+		evolved: false,
+		imagePath: "img/jelly.jpg"},
 
-	{name: 'Pieces',
-	description: "A favorite from Carmin-at-Sea.",
-	type: "candy",
-	evolved: false,
-	imagePath: "img/dots.jpg"},
-  ];
+		{name: 'Pieces',
+		description: "A favorite from Carmin-at-Sea.",
+		type: "candy",
+		evolved: false,
+		imagePath: "img/dots.jpg"},
+	  ];
 
-  $scope.feed = {
-  	name: '',
-  	candy: '',
-  	evolved: '',
-  }
-
-$scope.addItem = function(item){
-	$scope.inBag.push(item);
-}  
-
-$scope.removeItem = function(item) {
-	var index = $scope.inBag.indexOf(item);
-	if (index > -1) {
-    $scope.inBag.splice(index, 1);
+		//empty array records info from selected Pokemon to be fed
+	$scope.feed = {
+	  	name: '',
+	  	candy: '',
+	  	evolved: '',
 	}
-}
 
-$scope.feedWho = function(pokemon){
-	$scope.feedPoke = true;
-	$scope.feed.name = pokemon.name;
-	$scope.feed.evolved = pokemon.evolved;
-}
+	//adds new Pokemon/item to bag
+	$scope.addItem = function(item){
+		$scope.inBag.push(item);
+	}  
 
-$scope.feedWhat = function(candy){
-	if ($scope.feed.evolved === false) {
+	//removes Pokemon/item from bag
+	$scope.removeItem = function(item) {
+		var index = $scope.inBag.indexOf(item);
+		if (index > -1) {
+	    $scope.inBag.splice(index, 1);
+		}
+	}
 
-		for (var x=0; x < $scope.inBag.length; x++){	
-			if ($scope.inBag[x].name == $scope.feed.name){
-				$scope.inBag.splice(x, 1);
-				for (var y=0; y < $scope.inBag.length; y++){
-					if ($scope.inBag[y].name == candy.name){
-						$scope.inBag.splice(y, 1);
-					}				
+	//records relevant Pokemon to feed
+	$scope.feedWho = function(pokemon){
+		$scope.feedPoke = true;
+		$scope.feed.name = pokemon.name;
+		$scope.feed.evolved = pokemon.evolved;
+	}
+
+	//selects which candy from bag to feed to Pokemon
+	$scope.feedWhat = function(candy){
+		if ($scope.feed.evolved === false) { //feeds only non-evolved Pokemon
+
+			//iterate through bag, if item matches selected Pokemon, then remove from bag
+			for (var x=0; x < $scope.inBag.length; x++){	
+				if ($scope.inBag[x].name == $scope.feed.name){
+					$scope.inBag.splice(x, 1);
+					for (var y=0; y < $scope.inBag.length; y++){
+						if ($scope.inBag[y].name == candy.name){
+							$scope.inBag.splice(y, 1);
+						}				
+					}
 				}
 			}
-		}
-		 //for this assignment, let's assume all pokemon evolve to Mewtwo
-		alert("Your pokemon is evolving...");
-		alert("It's a Mewtwo!");
-		var mewtwo = 
-			{name: 'Mewtwo',
-			description: "The ultimate evolution",
-			type: "pokemon",
-			evolved: true,
-			imagePath: "img/mewtwo.jpg"};
+			 //for this assignment, let's assume all pokemon evolve to Mewtwo
+			alert("Your pokemon is evolving...");
+			alert("It's a Mewtwo!");
 
-		$scope.inBag.push(mewtwo);
+			//replaces Pokemon with new Mewtwo evolution, and adds to bag
+			var mewtwo = 
+				{name: 'Mewtwo',
+				description: "The ultimate evolution",
+				type: "pokemon",
+				evolved: true,
+				imagePath: "img/mewtwo.jpg"};
+
+			$scope.inBag.push(mewtwo);
+		}
+		
+		else {
+			alert("Your Pokemon has finished evolving. Try feeding another.")
+		}
 	}
-	
-	else {
-		alert("Your Pokemon has finished evolving. Try feeding another.")
-	}
-}
 
 })
 
-// $(document).ready(function(){
-// 	$('button').click(function(){
-// 	$(this).hide();
-// 	$(this).next().hide();
-// 	})	
-// })
